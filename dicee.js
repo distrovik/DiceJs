89364aaa259630ef604fcd0eaba5dfea95d292fb
V <mrirfanwork@gmail.com>
2023-02-03 12:49:47 +0800

    Complete Dice Game

---
+++ dicee.js
@@ -0,0 +1,16 @@
+var rando1 = Math.floor(Math.random() * 6) + 1;
+var rando2 = Math.floor(Math.random() * 6) + 1;
+
+var dice1 = "images/dice" + rando1 + ".png";
+var dice2 = "images/dice" + rando2 + ".png";
+
+document.querySelector(".img1").setAttribute("src", dice1);
+document.querySelector(".img2").setAttribute("src", dice2);
+
+if (rando1 === rando2) {
+  document.querySelector("h1").innerHTML = "Draw!👀 Refresh Again!";
+} else if (rando1 > rando2) {
+  document.querySelector("h1").innerHTML = "☝🏼 Player 1 Wins!";
+} else if (rando1 < rando2) {
+  document.querySelector("h1").innerHTML = "✌🏼 Player 2 Wins!";
+}
